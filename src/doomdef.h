bec460d06c0a91945bed1e8e57a7b5aef953a01e
Ian Burgmyer <spectere@gmail.com>
2017-03-27 10:21:16 -0400

    Updated copyright notice where appropriate.

--- src/doomdef.h
+++ src/doomdef.h
@@ -1,6 +1,7 @@
 //-----------------------------------------------------------------------------
 //
 // Copyright (C) 1993-1996 by id Software, Inc.
+// Copyright (C) 2017 by Ian Burgmyer
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
