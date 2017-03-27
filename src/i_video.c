d97e0c58013471795f48ecd0ae76256c348d0804
Ian Burgmyer <iburgmyer@classicoptical.com>
2017-03-27 10:32:27 -0400

    Split off event handling into a separate source file; moved I_StartTic into i_system, since it doesn't contain video code.

--- src/i_video.c
+++ src/i_video.c
@@ -25,7 +25,6 @@
 #include "doomstat.h"
 #include "i_system.h"
 #include "v_video.h"
-#include "d_main.h"
 
 #include "doomdef.h"
 
@@ -51,140 +50,11 @@ void I_ShutdownGraphics(void) {
 
 extern int scale;
 extern int capture_mouse;
-extern int mouse_sensitivity_multiplier_x;
-extern int mouse_sensitivity_multiplier_y;
 
 //
 // I_StartFrame
 //
 void I_StartFrame(void) {}
-
-static int MapSDLKey(SDL_Keysym keysym) {
-	/* Convert uppercase symbobls into lowercase. */
-	if(keysym.sym >= 'A' && keysym.sym <= 'Z')
-		return keysym.sym + 0x20;
-
-	/* Handles everything from Space to Tilde. */
-	if(keysym.sym >= 0x20 && keysym.sym <= 0x7E)
-		return keysym.sym;
-
-	/* Try to handle everything else. */
-	switch(keysym.sym) {
-	case SDLK_LCTRL: case SDLK_RCTRL:
-		return KEY_RCTRL;
-	case SDLK_LSHIFT: case SDLK_RSHIFT:
-		return KEY_RSHIFT;
-	case SDLK_LALT: case SDLK_RALT:
-		return KEY_RALT;
-	case SDLK_UP:
-		return KEY_UPARROW;
-	case SDLK_DOWN:
-		return KEY_DOWNARROW;
-	case SDLK_LEFT:
-		return KEY_LEFTARROW;
-	case SDLK_RIGHT:
-		return KEY_RIGHTARROW;
-	case SDLK_RETURN: case SDLK_RETURN2:
-		return KEY_ENTER;
-	case SDLK_BACKSPACE:	
-		return KEY_BACKSPACE;
-	case SDLK_ESCAPE:
-		return KEY_ESCAPE;
-	case SDLK_PAUSE:
-		return KEY_PAUSE;
-	case SDLK_TAB:
-		return KEY_TAB;
-	case SDLK_F1:
-		return KEY_F1;
-	case SDLK_F2:
-		return KEY_F2;
-	case SDLK_F3:
-		return KEY_F3;
-	case SDLK_F4:
-		return KEY_F4;
-	case SDLK_F5:
-		return KEY_F5;
-	case SDLK_F6:
-		return KEY_F6;
-	case SDLK_F7:
-		return KEY_F7;
-	case SDLK_F8:
-		return KEY_F8;
-	case SDLK_F9:
-		return KEY_F9;
-	case SDLK_F10:
-		return KEY_F10;
-	case SDLK_F11:
-		return KEY_F11;
-	case SDLK_F12:
-		return KEY_F12;
-	}
-
-	/* The player hit...something, I guess. */
-	return -1;
-}
-
-void I_GetEvent(void) {
-	event_t d_event;
-	SDL_Event sdl_event;
-	Uint32 button_mask;
-	SDL_bool dispatch_event;
-
-	/* TODO: Add gamepad/joystick support. */
-	while(SDL_PollEvent(&sdl_event)) {
-		dispatch_event = SDL_FALSE;
-
-		switch(sdl_event.type) {
-		case SDL_KEYDOWN:
-			d_event.type = ev_keydown;
-			break;
-		case SDL_KEYUP:
-			d_event.type = ev_keyup;
-			break;
-		case SDL_MOUSEMOTION:
-			button_mask = SDL_GetMouseState(NULL, NULL);
-
-			d_event.type = ev_mouse;
-			d_event.data1 = button_mask & SDL_BUTTON(SDL_BUTTON_LEFT) ? 1 : 0 |
-				button_mask & SDL_BUTTON(SDL_BUTTON_RIGHT) ? 2 : 0 |
-				button_mask & SDL_BUTTON(SDL_BUTTON_MIDDLE) ? 4 : 0;
-			d_event.data2 = sdl_event.motion.xrel << mouse_sensitivity_multiplier_x;
-			d_event.data3 = -sdl_event.motion.yrel << mouse_sensitivity_multiplier_y;
-			
-			dispatch_event = SDL_TRUE;
-			break;
-		case SDL_MOUSEBUTTONUP:
-		case SDL_MOUSEBUTTONDOWN:
-			button_mask = SDL_GetMouseState(NULL, NULL);
-
-			d_event.type = ev_mouse;
-			d_event.data1 = button_mask & SDL_BUTTON(SDL_BUTTON_LEFT) ? 1 : 0 |
-				button_mask & SDL_BUTTON(SDL_BUTTON_RIGHT) ? 2 : 0 |
-				button_mask & SDL_BUTTON(SDL_BUTTON_MIDDLE) ? 4 : 0;
-			d_event.data2 = d_event.data3 = 0;
-
-			dispatch_event = SDL_TRUE;
-			break;
-		default:
-			continue;
-		}
-
-		/* Handle keyboard events. */
-		if(d_event.type == ev_keydown || d_event.type == ev_keyup)
-			if((d_event.data1 = MapSDLKey(sdl_event.key.keysym)) != -1)
-				dispatch_event = SDL_TRUE;
-
-		/* Dispatch events, if applicable. */
-		if(dispatch_event) D_PostEvent(&d_event);
-	}
-}
-
-//
-// I_StartTic
-//
-void I_StartTic(void) {
-	I_GetEvent();
-}
 
 
 //
