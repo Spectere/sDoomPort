9d14064b194be1729bf90de389e63a757169b51a
Ian Burgmyer <spectere@gmail.com>
2017-04-03 15:35:30 -0400

    Restored the old FixedDiv2 function, merged it with FixedDiv, and replaced a magic number with a constant.

--- src/m_fixed.c
+++ src/m_fixed.c
@@ -17,6 +17,8 @@
 //
 //-----------------------------------------------------------------------------
 
+#include <SDL_stdinc.h>
+
 #include "stdlib.h"
 
 #include "doomtype.h"
@@ -30,7 +32,7 @@ fixed_t
 FixedMul
 (fixed_t a,
  fixed_t b) {
-	return ((long long) a * (long long) b) >> FRACBITS;
+	return (Sint64) a * (Sint64) b >> FRACBITS;
 }
 
 
@@ -42,27 +44,9 @@ fixed_t
 FixedDiv
 (fixed_t a,
  fixed_t b) {
-	if((abs(a) >> 14) >= abs(b))
+	Sint64 c;
+	if(abs(a) >> 14 >= abs(b))
 		return (a ^ b) < 0 ? MININT : MAXINT;
-	return FixedDiv2(a, b);
-}
-
-
-fixed_t
-FixedDiv2
-(fixed_t a,
- fixed_t b) {
-#if 0
-    long long c;
-    c = ((long long)a<<16) / ((long long)b);
-    return (fixed_t) c;
-#endif
-
-	double c;
-
-	c = ((double)a) / ((double)b) * FRACUNIT;
-
-	if(c >= 2147483648.0 || c < -2147483648.0)
-		I_Error("FixedDiv: divide by zero");
-	return (fixed_t) c;
+	c = ((Sint64)a << FRACBITS) / (Sint64)b;
+	return (fixed_t)c;
 }
