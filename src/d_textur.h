b9f15d86a45436f4f03de8b7bda63c1bbc5f99ce
Ian Burgmyer <iburgmyer@classicoptical.com>
2017-04-06 15:37:40 -0400

    Corrected missing include in d_textur.h

--- src/d_textur.h
+++ src/d_textur.h
@@ -1,6 +1,7 @@
 //-----------------------------------------------------------------------------
 //
 // Copyright (C) 1993-1996 by id Software, Inc.
+// Copyright (C) 2017 by Ian Burgmyer
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -22,8 +23,8 @@
 #ifndef __D_TEXTUR__
 #define __D_TEXTUR__
 
+#include <SDL_stdinc.h>
 #include "doomtype.h"
-
 
 //
 // Flats?
