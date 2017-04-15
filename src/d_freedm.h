e350722bf01c77628333947d52cfc7d0897c399f
Ian Burgmyer <spectere@gmail.com>
2017-04-15 02:06:04 -0400

    Added support for BFG edition WADs.

---
+++ src/d_freedm.h
@@ -0,0 +1,291 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) 2001-2017 Contributors to the Freedoom project
+// Copyright (C) 2017 Ian Burgmyer
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//	Freedoom level names
+//
+//-----------------------------------------------------------------------------
+
+#ifndef __D_FREEDM__
+#define __D_FREEDM__
+
+#define FREEDM1_E1M1 "C1M1: Outer Prison"
+#define FREEDM1_E1M2 "C1M2: Communications Center"
+#define FREEDM1_E1M3 "C1M3: Waste Disposal"
+#define FREEDM1_E1M4 "C1M4: Supply Depot"
+#define FREEDM1_E1M5 "C1M5: Main Control"
+#define FREEDM1_E1M6 "C1M6: Training Facility"
+#define FREEDM1_E1M7 "C1M7: Transportation Bay"
+#define FREEDM1_E1M8 "C1M8: Outpost Quarry"
+#define FREEDM1_E1M9 "C1M9: Armory"
+#define FREEDM1_E2M1 "C2M1: Ruins"
+#define FREEDM1_E2M2 "C2M2: Power Plant"
+#define FREEDM1_E2M3 "C2M3: Archaelogy Site"
+#define FREEDM1_E2M4 "C2M4: Sample Holding Site"
+#define FREEDM1_E2M5 "C2M5: Fortress 31"
+#define FREEDM1_E2M6 "C2M6: Trepidation Site"
+#define FREEDM1_E2M7 "C2M7: Quarantine Vessel"
+#define FREEDM1_E2M8 "C2M8: Containment Cell"
+#define FREEDM1_E2M9 "C2M9: Corruption of Man"
+#define FREEDM1_E3M1 "C3M1: Land of the Lost"
+#define FREEDM1_E3M2 "C3M2: Infernal Caverns"
+#define FREEDM1_E3M3 "C3M3: Derelict Temple"
+#define FREEDM1_E3M4 "C3M4: Sacrificial Bastion"
+#define FREEDM1_E3M5 "C3M5: Oblation Temple"
+#define FREEDM1_E3M6 "C3M6: Igneous Intrusion"
+#define FREEDM1_E3M7 "C3M7: No Regrets"
+#define FREEDM1_E3M8 "C3M8: Ancient Lair"
+#define FREEDM1_E3M9 "C3M9: Acquainted With Grief"
+#define FREEDM1_E4M1 "C4M1: Maintenance Area"
+#define FREEDM1_E4M2 "C4M2: Research Complex"
+#define FREEDM1_E4M3 "C4M3: Central Computing"
+#define FREEDM1_E4M4 "C4M4: Hydroponic Facility"
+#define FREEDM1_E4M5 "C4M5: Engineering Station"
+#define FREEDM1_E4M6 "C4M6: Command Center"
+#define FREEDM1_E4M7 "C4M7: Waste Treatment"
+#define FREEDM1_E4M8 "C4M8: Launch Bay"
+#define FREEDM1_E4M9 "C4M9: Operations"
+
+#define FREEDM2_1 "MAP01: Hydroelectric Plant"
+#define FREEDM2_2 "MAP02: Filtration Tunnels"
+#define FREEDM2_3 "MAP03: Crude Processing Center"
+#define FREEDM2_4 "MAP04: Containment Bay"
+#define FREEDM2_5 "MAP05: Sludge Burrow"
+#define FREEDM2_6 "MAP06: Gamma Labs"
+#define FREEDM2_7 "MAP07: Outer Storage Warehouse"
+#define FREEDM2_8 "MAP08: Astronomy Complex"
+#define FREEDM2_9 "MAP09: Datacenter"
+#define FREEDM2_10 "MAP10: Deadly Outlands"
+#define FREEDM2_11 "MAP11: Infinite Plain"
+#define FREEDM2_12 "MAP12: Railroads"
+#define FREEDM2_13 "MAP13: Station Earth"
+#define FREEDM2_14 "MAP14: Nuclear Zone"
+#define FREEDM2_15 "MAP15: Hostile Takeover"
+#define FREEDM2_16 "MAP16: Urban Jungle"
+#define FREEDM2_17 "MAP17: City Capitol"
+#define FREEDM2_18 "MAP18: Aquatics Lab"
+#define FREEDM2_19 "MAP19: Sewage Control"
+#define FREEDM2_20 "MAP20: Blood Ember Fortress"
+#define FREEDM2_21 "MAP21: Under Realm"
+#define FREEDM2_22 "MAP22: Remanasu"
+#define FREEDM2_23 "MAP23: Underground Facility"
+#define FREEDM2_24 "MAP24: Tertiary Loading Bay"
+#define FREEDM2_25 "MAP25: Red Works"
+#define FREEDM2_26 "MAP26: Dark Depths"
+#define FREEDM2_27 "MAP27: Warped Elementality"
+#define FREEDM2_28 "MAP28: Grim Redoubt"
+#define FREEDM2_29 "MAP29: Last Stand"
+#define FREEDM2_30 "MAP30: Jaws of Defeat"
+#define FREEDM2_31 "MAP31: Be Quiet"
+#define FREEDM2_32 "MAP32: Not Sure"
+
+#define FREEDM1_E1TEXT \
+        "You've made it out of the outpost.\n"\
+        "But you're still stuck on this rock.\n"\
+        "AGM sure has been busy with their\n"\
+        "\"experiments\", with those monsters\n"\
+        "infesting that dreadful place. You are\n"\
+        "an experiment as well, so who cares?\n"\
+        "There weren't any ships inside the\n"\
+        "outpost. The only section that might\n"\
+        "have one is the Military Labs. The\n"\
+        "very place you got your strength\n"\
+        "from. Damned AGM scientists.\n\n"\
+        "This nightmare is too much. You\n"\
+        "step through the massive doors of\n"\
+        "the Military Labs.\n\n"\
+        "There could be a way to escape in there."
+
+#define FREEDM1_E2TEXT \
+        "The massive brutes collapse onto\n"\
+        "the ground. They fall apart bit by bit.\n"\
+        "Unfortunately, there was a no ship\n"\
+        "here either. The Labs have bizarre\n"\
+        "technology though. Like this one.\n\n"\
+        "A huge teleporter machine, set for...\n"\
+        "A place called Horizon?\n\n"\
+        "You never heard of such thing before.\n"\
+        "Maybe it's another AGM facility.\n"\
+        "Meaning there could be a ship on\n"\
+        "the other side. Sounds like a good\n"\
+        "plan. Or at least, as good as it gets.\n\n"\
+        "Next stop, Horizon."
+
+#define FREEDM1_E3TEXT \
+        "The abomination explodes into\n"\
+        "oblivion. That's that. Horizon is a\n"\
+        "whole new world, not a facility.\n"\
+        "Even worse than the outpost you\n"\
+        "rotted away for years in. Meaning\n"\
+        "you're still trapped.\n\n"\
+        "An AGM teleporter is here somehow.\n"\
+        "They must have been here then.\n"\
+        "The teleporter is set for a facility\n"\
+        "called \"Double Impact\", which is a\n"\
+        "ridiculous name. Who cares? All\n"\
+        "you need is a working ship.\n\n"\
+        "You drop all your weapons on\n"\
+        "the ground and step on the platform,\n"\
+        "reintegrating on Double Impact."
+
+#define FREEDM1_E4TEXT \
+        "Despite fighting an army of monsters\n"\
+        "you managed to find a ship here.\n"\
+        "Looks like freedom is yours.\n\n"\
+        "\"Time to get out of here.\"\n"\
+        "You tell yourself and plump down\n"\
+        "on the comfortable chair.\n\n"\
+        "The ship rumbles as she wakes up.\n"\
+        "As you lift off, you think of Earth.\n\n"\
+        "Hopefully, AGM won't find you there.\n"\
+        "They've got the outbreak to deal\n"\
+        "with, so that'll give you some time.\n\n"\
+        "Destination: Earth"
+
+/* After MAP06, before MAP07: */
+#define FREEDM2_C1TEXT \
+        "You've found your way to the\n"\
+        "complex's outer storage warehouse.\n"\
+        "Not even Earth was safe from the\n"\
+        "monsters. This AGM space terminal\n"\
+        "is a lost cause. Best to get out.\n\n"\
+        "You press the panel to call the lift.\n"\
+        "It slowly lowers, then speeds up.\n"\
+        "Growls come along with it.\n"\
+        "Looks like you're not alone here.\n\n"\
+        "Planted your feet.\n\n"\
+        "Checked your weapons.\n\n"\
+        "Time to go up."
+
+/* After MAP11, before MAP12: */
+#define FREEDM2_C2TEXT \
+        "The facility is gone, no doubt there.\n"\
+        "You didn't find anyone alive.\n"\
+        "But you don't want to keep fighting\n"\
+        "either. You are only human.\n"\
+        "There's a teleporter here, set for\n"\
+        "a city unknown to you. City means\n"\
+        "people, right?\n\n"\
+        "The bolts holding the platform\n"\
+        "flies off over the ledge. The place\n"\
+        "is falling apart. You quickly step on\n"\
+        "the platform and reappear at the city\n"\
+        "It's been destroyed to the ground.\n\n"\
+        "You can't go back either."
+
+/* After MAP20, before MAP21: */
+#define FREEDM2_C3TEXT \
+        "There is no AGM here. no humans.\n"\
+        "Just monsters and monsters.\n"\
+        "And you.\n"\
+        "This could be the beginning of\n"\
+        "your freedom.\n"\
+        "The finale of this endless fighting\n"\
+        "against the monsters.\n"\
+        "No man has ever survived in this\n"\
+        "wasteland.\n\n"\
+        "Other than you, that is.\n"\
+        "Maybe AGM saw this place.\n"\
+        "But never got through here.\n"\
+        "Either way, it doesn't matter.\n\n"\
+        "You're stuck here once again."
+
+/* After MAP30 (endgame text): */
+#define FREEDM2_C4TEXT \
+        "The evil thing becomes unstable.\n\n"\
+        "Its final roars echoes throughout the\n"\
+        "room till it crumples into scrap metal.\n"\
+        "A portal opens up, and in it, you see a\n"\
+        "small town. You jump in quickly.\n"\
+        "AGM learned its lesson this time.\n\n"\
+        "Hopefully.\n\n"\
+        "No one will know know who saved them.\n\n"\
+        "No one will know what happened here.\n\n"\
+        "No one will ever find you again."
+
+/* Before MAP31 (secret level #1): */
+#define FREEDM2_C5TEXT \
+        "You step onto the machine and\n"\
+        "after a strange flash you find\n"\
+        "yourself in a rusty cage.\n\n"\
+        "\"What is this place?\"\n\n"\
+        "Before you can wonder further\n"\
+        "you see monsters all around\n"\
+        "you. It's a trap.\n\n"\
+        "You silently bring out\n"\
+        "your gun, ready to kill."
+
+/* Before MAP32 (secret level #2): */
+#define FREEDM2_C6TEXT \
+        "The world dissipitates around\n"\
+        "you. You got out of that trap. Only\n"\
+        "to find yourself inside another.\n"\
+        "As the glow of the teleporter\n"\
+        "subsides, it looks like you may\n"\
+        "have made a wrong turn. Again.\n\n"\
+        "\"Not again.\"\n\n"\
+        "The world comes back to you.\n"\
+        "The monsters you fought before\n"\
+        "raise their weapons,\n\n"\
+        "aiming for your head."
+
+#define FD_ZOMBIE	"zombie"
+#define FD_SHOTGUN	"shotgun zombie"
+#define FD_HEAVY	"minigun zombie"
+#define FD_IMP		"serpentipede"
+#define FD_DEMON	"flesh worm"
+#define FD_LOST		"deadflare"
+#define FD_CACO		"trilobite"
+#define FD_HELL		"pain bringer"
+#define FD_BARON	"pain lord"
+#define FD_ARACH	"technospider"
+#define FD_PAIN		"summoner"
+#define FD_REVEN	"dark soldier"
+#define FD_MANCU	"combat slug"
+#define FD_ARCH		"flame bringer"
+#define FD_SPIDER	"large technospider"
+#define FD_CYBER	"assault tripod"
+#define FD_HERO		"savior of humanity"
+
+#define FD_GOTBFG9000	"the SKAG 1337 ... time to kick some ass!"
+#define FD_GOTCHAINGUN	"you got the minigun!"
+#define FD_GOTCHAINSAW	"you got the angle grinder!"
+#define FD_GOTLAUNCHER	"you got the missile launcher!"
+#define FD_GOTPLASMA	"you got the polaric energy cannon!"
+#define FD_GOTSHOTGUN	"you got the pump-action shotgun!"
+#define FD_GOTSHOTGUN2	"you got the double-barrelled shotgun!"
+
+#define FD_GOTCLIP		"picked up an ammo clip."
+#define FD_GOTCLIPBOX	"picked up a box of ammo."
+#define FD_GOTROCKET	"picked up a missile."
+#define FD_GOTROCKBOX	"picked up a crate of missiles."
+#define FD_GOTCELL		"picked up a small energy recharge."
+#define FD_GOTCELLBOX	"picked up a large energy recharge."
+#define FD_GOTSHELLS	"picked up some shotgun shells."
+#define FD_GOTSHELLBOX	"picked up a box of shotgun shells."
+#define FD_GOTBACKPACK	"you found a backpack!"
+
+#define FD_BGFLATE1		"AQF051"
+#define FD_BGFLATE2		"AQF054"
+#define FD_BGFLATE3		"FLAT5_2"
+#define FD_BGFLATE4		"AQF075"
+#define FD_BGFLAT06		"AQF016"
+#define FD_BGFLAT11		"AQF001"
+#define FD_BGFLAT20		"FLAT5_6"
+#define FD_BGFLAT30		"SLIME13"
+#define FD_BGFLAT15		"AQF004"
+#define FD_BGFLAT31		"AQF021"
+
+#endif /* __D_FREEDM__ */
