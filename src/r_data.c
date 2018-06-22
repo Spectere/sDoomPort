a9f7873a0d535d18134842d58fdeb32215bf32b0
Ian Burgmyer <spectere@gmail.com>
2018-06-22 16:27:22 -0400

    Silly change: fixed the progress bar for 'Doom refresh daemon.'

--- src/r_data.c
+++ src/r_data.c
@@ -452,10 +452,10 @@ void R_InitTextures(void) {
 	printf("[");
 	for(i = 0; i < temp3; i++)
 		printf(" ");
-	printf("         ]");
+	printf("]");
 	for(i = 0; i < temp3; i++)
 		printf("\x8");
-	printf("\x8\x8\x8\x8\x8\x8\x8\x8\x8\x8");
+	printf("\x8");
 
 	for(i = 0; i < numtextures; i++ , directory++) {
 		if(!(i & 63))
